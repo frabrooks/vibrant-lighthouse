6afbbc53f5c51bf28910941bcffcbc590d25e051
FraserBrooks <fraserliambrooks@outlook.com>
2022-08-03 18:11:12 +0100

    Add 'certbot renew' to cron script

--- certbot/cert-renew-script.sh
+++ certbot/cert-renew-script.sh
@@ -1,2 +1,8 @@
 #!/bin/sh
 echo "Current date and time is " "$(date +%D-%H:%M)"
+
+echo "Running 'certbot renew', time is " "$(date +%D-%H:%M)" >> /var/log/letsencrypt/letsencrypt.log
+
+certbot renew
+
+echo "Ran 'certbot renew', time is " "$(date +%D-%H:%M)" >> /var/log/letsencrypt/letsencrypt.log
