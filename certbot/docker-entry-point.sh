7795760b54a84832eef9c80ed84877e85bf461e5
FraserBrooks <fraserliambrooks@outlook.com>
2022-09-18 20:26:46 +0100

    Add shebang

--- certbot/docker-entry-point.sh
+++ certbot/docker-entry-point.sh
@@ -1,4 +1,5 @@
-certbot certonly --webroot -w /var/www/certbot/ --non-interactive --agree-tos --domains eisentodo.com --email fraserliambrooks@outlook.com;
-certbot certonly --webroot -w /var/www/certbot/ --non-interactive --agree-tos --domains api.eisentodo.com --email fraserliambrooks@outlook.com;
-cp /ssl.conf /etc/letsencrypt/;
+#!/bin/sh
+certbot certonly --webroot -w /var/www/certbot/ --non-interactive --agree-tos --domains eisentodo.com --email fraserliambrooks@outlook.com
+certbot certonly --webroot -w /var/www/certbot/ --non-interactive --agree-tos --domains api.eisentodo.com --email fraserliambrooks@outlook.com
+cp /ssl.conf /etc/letsencrypt/
 crond -f
