76d74bd31363150d71c382336f78ddef8ae187e9
FraserBrooks <fraserliambrooks@outlook.com>
2022-09-25 14:04:57 +0100

    Use environment.ts for API URL

--- ng-app/src/app/todo-test/todo-test.component.ts
+++ ng-app/src/app/todo-test/todo-test.component.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { FormControl } from '@angular/forms';
+import { environment } from 'src/environments/environment';
 import { Todo } from '../todo.interface';
 
 @Component({
@@ -18,17 +19,17 @@ export class TodoTestComponent implements OnInit {
   constructor(private http: HttpClient) { }
 
   ngOnInit(): void {
-    this.http.get<string>('https://api.eisentodo.com/').subscribe((status: string) => this.status = status);
-    this.http.get<Array<Todo>>('https://api.eisentodo.com/todo').subscribe((todos: Array<Todo>) => this.todos = todos);
+    this.http.get<string>(`${environment.apiurl}`).subscribe((status: string) => this.status = status);
+    this.http.get<Array<Todo>>(`${environment.apiurl}/todo`).subscribe((todos: Array<Todo>) => this.todos = todos);
   }
 
   public getTodos(): void {
-    this.http.get<Array<Todo>>('https://api.eisentodo.com/todo').subscribe((todos: Array<Todo>) => this.todos = todos);
+    this.http.get<Array<Todo>>(`${environment.apiurl}/todo`).subscribe((todos: Array<Todo>) => this.todos = todos);
   }
 
   public postTodo(title: string): void {
     if (! this.newTodo) return;
-    this.http.post<Todo>('https://api.eisentodo.com/todo', { title: title, due_soon: false, important: false, archived: false }).subscribe(
+    this.http.post<Todo>(`${environment.apiurl}/todo`, { title: title, due_soon: false, important: false, archived: false }).subscribe(
       () => {
         this.newTodo = "";
         this.getTodos();
